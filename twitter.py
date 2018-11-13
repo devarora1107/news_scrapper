bd4fd3a0a11736915fc11937182a1057efa75f8b
devarora arora <devarora@outlook.com>
2018-11-13 16:01:12 +0530

    asd

--- twitter.py
+++ twitter.py
@@ -5,7 +5,7 @@ import os
 import apiConfig
 api=apiConfig.get_config()
 global searchItem
-searchItem='RBI'
+searchItem='unity'
 ####input your credentials here
 
 #####United Airlines
@@ -29,7 +29,31 @@ def sortHashtag(string):
         hashtags[string]=1
     
     
+def parseUserDetails(text):
+    result=[]
+    text=text.split(' ')
+    for t in text:
+        try:
+            if(t[0]=='@'):
+                result.append(t)
+            else:
+                pass
+        except:
+            return result
+    return result
+def parseLinkDetails(text):
+    result=[]
+    text=text.split(' ')
+    for t in text:
+        try:
+            if(re.match('^https',text)):
+                result.append(t)
+            else:
+                pass
 
+        except:
+            pass
+    return result
 def parseTextHashtags(text):
     result=[]
     text=text.split(' ')
@@ -43,15 +67,15 @@ def parseTextHashtags(text):
         sortHashtag(hashtag)
     return result
 
-for tweet in tweepy.Cursor(api.search,q=searchItem,count=100,
-                           lang="en",
-                           since="2018-11-03").items():
-    #print (tweet.created_at, tweet.text)
-    parseText=parseTextHashtags(tweet.text.encode('utf-8'))
-    #print parseText
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),parseText] )
+for tweet in tweepy.Cursor(api.search,q=searchItem,count=100,lang="en",since="2018-11-11").items():
+    print (tweet.created_at, tweet.text)
+    parseHashTags=parseTextHashtags(tweet.text.encode('utf-8'))
+    parseUser=parseUserDetails(tweet.text.encode('utf-8'))
+    parseLink=parseLinkDetails(tweet.text.encode('utf-8'))
+    
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),parseHashTags,parseUser,parseLink] )
 
-
+    ##print tweet.text.encode('utf-8')
 
 for key in hashtags.keys():
     csvHashtagWriter.writerow([key,hashtags[key]])
